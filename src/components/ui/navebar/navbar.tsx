4252df74a64f4fffcbf86b945134d56ba050329d
Hares <alharsb15@gmail.com>
2025-03-17 05:32:46 +0300

    s

--- src/components/ui/navebar/navbar.tsx
+++ src/components/ui/navebar/navbar.tsx
@@ -6,9 +6,9 @@ import Button from "../button";
 export const Navbar = () => {
     return (
         <Fragment>
-            <div className="menu w-[278px] h-screen flex flex-col justify-between py-6 relative bg-white border-e px-[15px]">
+            <div className="menu sm:w-[278px] w-[62px] overflow-hidden h-screen flex flex-col justify-between py-6 relative bg-white border-e px-[15px]">
                 <ul className="flex flex-col items-start gap-[10px] ">
-                    <li className="text-mainBlue font-bold text-lg ps-4">
+                    <li className="text-mainBlue font-bold text-lg ps-4 max-sm:overflow-hidden">
                         logo
                     </li>
                     {navbarArray.map((link) => {
@@ -18,7 +18,7 @@ export const Navbar = () => {
                 <Button className="!justify-start">
                     <span className="flex gap-2 items-center">
                         <LogoutIcon />
-                        <span className="text-[14px] font-[500] text-main ">
+                        <span className="text-[14px]  font-[500] text-main ">
                             Log out
                         </span>
                     </span>
